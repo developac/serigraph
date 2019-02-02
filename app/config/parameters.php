2b205c5dce9244993af605ff2cf7e40997954b66
ciro <ciroperna@libero.it>
2019-02-02 17:18:19 +0100

    Primo commit

---
+++ app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => 'localhost',
+    'database_port' => '',
+    'database_name' => 'serigrap_db',
+    'database_user' => 'serigrap_user',
+    'database_password' => 'HWzwCC3u4NEl',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'R42Cxf8DJaJ3HBPc0279En1eu8baTdKN2yaeX9kQ27qcZQXLdqCVctuu',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-08-20',
+    'locale' => 'it-IT',
+    'cookie_key' => '7ggRu6C7u09XhEFMNAvGV8dO06PcfYcj6bOdMwg5LHZ1meHRhhnFZrO5',
+    'cookie_iv' => 'DgvAUTk7',
+    'new_cookie_key' => 'def00000327709cc9c69ff5b537099426f5027496c587937f3c8ffe7d0b52741bd3262ebba73919cd27e109e4d9551965db3dddd46568836fa26a3a0683f911d38f4c14a',
+  ),
+);
